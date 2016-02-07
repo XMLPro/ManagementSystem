5adde6d27022b31aabca612466f6290c40027a0b
sinshutukibotu <c0114292a2@edu.teu.ac.jp>
2016-02-07 16:23:57 +0900

    /adminで作成したデータベースを参照出きるようにした

--- system/admin.py
+++ system/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Equipment, Search, Reserved, Request, Vote, LendingHistory
 
 admin.site.register(User)
+admin.site.register(Equipment)
+admin.site.register(Search)
+admin.site.register(Reserved)
+admin.site.register(Request)
+admin.site.register(Vote)
+admin.site.register(LendingHistory)
+
+
+
+
+
+
+
