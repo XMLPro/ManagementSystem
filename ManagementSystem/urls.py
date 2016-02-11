e6c871f77e181f7a3fdab9bec47592d912540e6f
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-02-11 15:51:29 +0900

    controllerの分割　場所: system/controllers/ - ManagementSystem/urls.py のtopViewのurl修正 - views.py からtopViewのcontrollerを削除 - system/controllers/topView.py topViewのcontrollerを作成

--- ManagementSystem/urls.py
+++ ManagementSystem/urls.py
@@ -16,6 +16,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^system/$', 'system.views.topView'),
+    url(r'^system/$', 'system.controllers.topView.topView'),
     url(r'^admin/', include(admin.site.urls)),
 ]
---
+++ system/controllers/topView.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render_to_response
+from system.models import Equipment, Reserved
+
+def topView(request):
+    equipment_list = Equipment.objects.all()
+    for equipment in equipment_list:
+        equipment.reserved_num = Reserved.objects.filter(equipment=equipment).count()
+        print(equipment.reserved_num)
+    return render_to_response('topView.html',{
+        'equipment_list': equipment_list,
+        })
