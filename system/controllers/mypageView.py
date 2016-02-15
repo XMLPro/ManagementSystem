c92df350c087760115964bcf87d6c7f1c52861e0
sinshutukibotu <c0114292a2@edu.teu.ac.jp>
2016-02-15 20:54:22 +0900

    add Myページ

--- system/controllers/mypageView.py
+++ system/controllers/mypageView.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render_to_response
-from system.models import *
+from system.models import CustomUser, Equipment, Reserved
 
 def mypageView(request):
-    my_list = []
-    my_reserved = []
+    borrower_list = Equipment.objects.filter(borrower=request.user.id)
+    for borrower_item in borrower_list:
+        count = Reserved.objects.filter(equipment=borrower_list).count()
+        setattr(borrower_item, 'num', count)
+    reserved_list = Reserved.objects.filter(user=request.user.id)
     return render_to_response('mypageView.html',{
-        'my_list': my_list,
-        'my_reserved': my_reserved,
+        'borrower_list': borrower_list,
+        'reserved_list': reserved_list,
         })
-
