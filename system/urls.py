ad16a92dc4cad999e3097fe9604ad27c320b2bd3
Takayuki Fuzita <c0114451d5@edu.teu.ac.jp>
2016-02-15 18:51:37 +0900

    correct

--- system/urls.py
+++ system/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     url(r'^$', topView.topView),
     url(r'^mypage/$', mypageView.mypageView),
     url(r'^log/$', logView.logView),
-    url(r'^request/$', requestView.requestView),
-    url(r'^login/$', 'django.contrib.auth.views.login',
-        {'template_name': 'login.html'}),
-    url(r'^logout/$', 'django.contrib.auth.views.logout',
-        {'template_name': 'logout.html'}),
+    url(r'^request/$', requestView.requestView, name='requestView'),
+    url(r'^vote/$', requestView.vote, name='vote'),
 ]
