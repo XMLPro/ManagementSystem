32e3fdf06a2a3d75e682e37a55e379dc249bd20f
sinshutukibotu <c0114292a2@edu.teu.ac.jp>
2016-02-16 12:46:33 +0900

    fix borrower_list -> borrower_item

--- system/controllers/mypageView.py
+++ system/controllers/mypageView.py
@@ -4,7 +4,7 @@ from system.models import CustomUser, Equipment, Reserved
 def mypageView(request):
     borrower_list = Equipment.objects.filter(borrower=request.user.id)
     for borrower_item in borrower_list:
-        count = Reserved.objects.filter(equipment=borrower_list).count()
+        count = Reserved.objects.filter(equipment=borrower_item).count()
         setattr(borrower_item, 'num', count)
     reserved_list = Reserved.objects.filter(user=request.user.id)
     return render_to_response('mypageView.html',{
