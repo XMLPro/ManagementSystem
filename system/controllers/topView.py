fa3082692b402d11378ddb550ea9dde7ed66346f
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-02-18 01:12:52 +0900

    topViewのボタンの状態変化を変更 - 済ボタンを追加 - 借りられるタイミング変更 - topView.pyにコメント追加

--- system/controllers/topView.py
+++ system/controllers/topView.py
@@ -1,13 +1,62 @@
 from django.shortcuts import render_to_response
 from system.models import Equipment, Reserved
 from django.template import RequestContext
+class Button():
+    """ topView.htmlで使用されている、備品の貸出制御ボタン """
+    def __init__(self, url, name):
+        self.url = url
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+# Buttonクラスを作成する関数
+# これらはcreate_buttonから使用される
+def create_borrow_button():
+    return Button("", "借")
+def create_reserve_button():
+    return Button("", "予")
+def create_return_button():
+    return Button("", "返")
+def create_finish_button():
+    return Button("", "済")
+
+def create_button(equipment, username):
+    # [ 返えす ]
+    # 備品を借りている人の名前と、自分の名前が一致
+    if equipment.borrower == username:
+        return create_return_button()
+    reserved = Reserved.objects.filter(equipment = equipment)
+
+    # [ 借りる ]
+    # 予約者がおり、自分の名前と一致
+    if reserved and reserved[0].user == username:
+        return create_borrow_button()
+
+    # [ 済み ]
+    # 予約者に自分の名前が含まれている
+    if str(username) in [str(x.user) for x in reserved]:
+        return create_finish_button()
+
+    # [ 借りる ]
+    # 借りているがいない
+    if equipment.borrower == None:
+        return create_borrow_button()
+
+    # [ 予約 ]
+    # それ以外
+    return create_reserve_button()
 
 def topView(request):
     ctxt = RequestContext(request, {})
     equipment_list = Equipment.objects.all()
+    # equipmentにフィールド追加
+    # .reserved_num: 予約者人数
+    # .button:       備品の貸出制御ボタン
     for equipment in equipment_list:
         equipment.reserved_num = Reserved.objects.filter(equipment=equipment).count()
-        # print(equipment.reserved_num)
+        equipment.button = create_button(equipment, request.user)
+
     return render_to_response('topView.html', {
         'equipment_list': equipment_list,
         'username': request.user,
