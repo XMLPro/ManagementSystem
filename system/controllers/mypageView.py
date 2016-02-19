b1d5491b777e1c731ec19d7089c9f2f505421996
Takayuki Fuzita <c0114451d5@edu.teu.ac.jp>
2016-02-20 04:09:56 +0900

    ni

--- system/controllers/mypageView.py
+++ system/controllers/mypageView.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render_to_response
 from system.models import CustomUser, Equipment, Reserved
 
+
 def mypageView(request):
     borrower_list = Equipment.objects.filter(borrower=request.user.id)
     for borrower_item in borrower_list:
-        count = Reserved.objects.filter(equipment=borrower_list).count()
+        print(borrower_list)
+        count = Reserved.objects.filter(equipment=borrower_item).count()
         setattr(borrower_item, 'num', count)
+
     reserved_list = Reserved.objects.filter(user=request.user.id)
-    return render_to_response('mypageView.html',{
+    return render_to_response('mypageView.html', {
         'borrower_list': borrower_list,
         'reserved_list': reserved_list,
-        })
+    })
