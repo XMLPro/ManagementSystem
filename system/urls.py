d343709845a39793b3be7d01ba6a457824c649ed
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-02-24 00:16:34 +0900

    ユーザー登録用のurlを2つ追加 - user_register : ユーザー登録用 - user_register/finish : ユーザー登録完了用

--- system/urls.py
+++ system/urls.py
@@ -4,16 +4,21 @@ from system.controllers import mypageView
 from system.controllers import logView
 from system.controllers import requestView
 from system.controllers import borrowReturnPost, postFinishView
-from system.controllers import user_registerView
+from system.controllers import userRegister
 
 urlpatterns = [
     url(r'^$', topView.topView, name='top'),
-    url(r'^user_register/$', user_registerView.user_register, name='user_register'),
+    url(r'^user_register/$', userRegister.userRegister,
+        name='user_register'),
+    url(r'^user_register/finish$', userRegister.finishUserRegisterView,
+        name='finish_register'),
     url(r'^mypage/$', mypageView.mypageView),
     url(r'^log/$', logView.logView),
     url(r'^request/$', requestView.requestView, name='requestView'),
     url(r'^vote/$', requestView.vote, name='vote'),
-    url(r'^manage/borrow/$', borrowReturnPost.borrowPost, name='manage-borrow'),
-    url(r'^manage/return/$', borrowReturnPost.returnPost, name='manage-return'),
+    url(r'^manage/borrow/$', borrowReturnPost.borrowPost,
+        name='manage-borrow'),
+    url(r'^manage/return/$', borrowReturnPost.returnPost,
+        name='manage-return'),
     url(r'^manage/$', postFinishView.postFinishView, name='manage'),
 ]
