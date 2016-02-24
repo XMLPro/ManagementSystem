d3c66c5ea5b85649fc0ef4cd4cc1ec87947ae734
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-02-24 19:03:10 +0900

    正常にpostを完了できなかった場合のリダイレクト先を自由に決められるように変更 - urlにbackname=[urlname]を指定するとそこにリダイレクトするようにしました。

--- system/controllers/reservePost.py
+++ system/controllers/reservePost.py
@@ -2,9 +2,18 @@ from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 from system.controllers.postFinishView import finish
 from system.models import Reserved, Equipment
+from django.http import Http404
+
+
+def reverse_or_404(default_name, name=None):
+    try:
+        return reverse(name if name else default_name)
+    except:
+        raise Http404
 
 
 def reservePost(request):
+    backname = request.GET.get("backname")
     if request.method == "POST":
         equipment = Equipment.objects.get(id=request.POST["equipment_id"])
         if not Reserved.objects.filter(
@@ -12,14 +21,15 @@ def reservePost(request):
             reserve = Reserved(equipment=equipment, user=request.user)
             reserve.save()
             return finish("reserve")
-    return redirect(reverse("system:top"))
+    return redirect(reverse_or_404("system:top", backname))
 
 
 def cancelPost(request):
+    backname = request.GET.get("backname")
     if request.method == "POST":
         equipment = Equipment.objects.get(id=request.POST["equipment_id"])
         reserved = Reserved.objects.get(equipment=equipment, user=request.user)
         if reserved:
             reserved.delete()
         return finish("cancel_reserve")
-    return redirect(reverse("system:top"))
+    return redirect(reverse_or_404("system:top", backname))
