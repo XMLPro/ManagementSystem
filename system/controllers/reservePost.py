53b797fa6583e2ac865dfabac8d017e18a4b1bde
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-02-28 20:55:35 +0900

    リダイレクト先を変更出きるように修正 - リダイレクト先の変更はurlのパラーメタから

--- system/controllers/reservePost.py
+++ system/controllers/reservePost.py
@@ -6,19 +6,25 @@ from system.controllers.utils import reverse_or_404
 
 
 def reservePost(request):
+    # リダイレクト先の受け取り
     backname = request.GET.get("backname")
+    backtitle = request.GET.get("backtitle")
+
     if request.method == "POST":
         equipment = Equipment.objects.get(id=request.POST["equipment_id"])
         if not Reserved.objects.filter(
                 equipment=equipment, user=request.user).exists():
             reserve = Reserved(equipment=equipment, user=request.user)
             reserve.save()
-            return finish("reserve")
+            return finish("reserve", backname, backtitle)
     return redirect(reverse_or_404("system:top", backname))
 
 
 def cancelPost(request):
+    # リダイレクト先の受け取り
     backname = request.GET.get("backname")
+    backtitle = request.GET.get("backtitle")
+
     if request.method == "POST":
         reserved_id = request.POST.get("reserved_id")
         if reserved_id:
@@ -28,5 +34,5 @@ def cancelPost(request):
             reserved = Reserved.objects.get(equipment=equipment, user=request.user)
         if reserved:
             reserved.delete()
-        return finish("cancel_reserve")
+        return finish("cancel_reserve", backname, backtitle)
     return redirect(reverse_or_404("system:top", backname))
