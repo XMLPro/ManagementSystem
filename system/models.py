ada33d1a383d11a5e64767166f2bb23744269fa7
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-03-19 22:01:51 +0900

    余分なコメント文を削除

--- system/models.py
+++ system/models.py
@@ -17,7 +17,6 @@ class Equipment(models.Model):
     author = models.CharField(max_length=50)
     company = models.CharField(max_length=50)
     price = models.IntegerField()
-    # information = models.CharField(max_length=10000)
     product_code = models.IntegerField()
     product_url = models.CharField(max_length=200)
     image_url = models.CharField(max_length=200, null=True, blank=True)
